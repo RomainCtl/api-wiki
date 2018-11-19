18a96d32c4aa1b146b4a43b8ba0b00c57798b09f
RomainCtl <romain.chantrel35@gmail.com>
2018-11-19 20:49:54 +0100

    generic logger for each api request

--- src/routes.php
+++ src/routes.php
@@ -3,41 +3,52 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-require "../src/resources/File.php";
+require "../src/utils/main.php";
 
 // Routes
 
 // GET File
 $app->get('/file', function(Request $request, Response $response){
-    $file = new File();
-    return call_user_func_array(array($this->response, "withJson"), $file->get("home"));
+    return call_user_func_array(
+        array($this->response, "withJson"),
+        api_logger($this, "GET", array("home"))
+    );
 });
 $app->get('/file/{filename_path}', function(Request $request, Response $response, array $args){
-    $file = new File();
-    $this->logger->info($args['filename_path']);
-    return call_user_func_array(array($this->response, "withJson"), $file->get($args['filename_path']));
+    return call_user_func_array(
+        array($this->response, "withJson"),
+        api_logger($this, "GET", $args)
+    );
 });
 $app->get('/file/{filename_path}/childs/{nb}', function(Request $request, Response $response, array $args){
-    $file = new File();
-    return call_user_func_array(array($this->response, "withJson"), $file->get($args['filename_path'], $args['nb']));
+    return call_user_func_array(
+        array($this->response, "withJson"),
+        api_logger($this, "GET", $args)
+    );
 });
 
 // POST File
 $app->post('/file', function(Request $request, Response $response){
     $input = $request->getParsedBody();
-    $file = new File();
-    return call_user_func_array(array($this->response, "withJson"), $file->post($input));
+    return call_user_func_array(
+        array($this->response, "withJson"),
+        api_logger($this, "POST", array($input))
+    );
 });
 
 // PUT File
 $app->put('/file/{filename_path}', function(Request $request, Response $response, array $args){
     $input = $request->getParsedBody();
-    $file = new File();
-    return call_user_func_array(array($this->response, "withJson"), $file->put($args['filename_path'], $input));
+    return call_user_func_array(
+        array($this->response, "withJson"),
+        api_logger($this, "PUT", array($args['filename_path'], $input))
+    );
 });
 
 // DELETE File
 $app->delete('/file/{filename_path}', function(Request $request, Response $response, array $args){
-    $file = new File();
-    return call_user_func_array(array($this->response, "withJson"), $file->delete($args['filename_path']));
+    return call_user_func_array(
+        array($this->response, "withJson"),
+        api_logger($this, "DELETE", $args)
+    );
 });
--- src/utils/main.php
+++ src/utils/main.php
@@ -1,13 +1,43 @@
 <?php
 
-function check_input(array $required, array $data_keys){
-    /**
-     * Check difference between two array
-     *
-     * @param array $required required attribute
-     * @param array $data_keys gived attribute
-     *
-     * @return bool true if $required == $data_keys else false
-     */
-    return array_diff($required, $data_keys) == array_diff($data_keys, $required);
+if (!function_exists("check_input")) {
+    function check_input(array $required, array $data_keys){
+        /**
+         * Check difference between two array
+         *
+         * @param array $required required attribute
+         * @param array $data_keys gived attribute
+         *
+         * @return bool true if $required == $data_keys else false
+         */
+        return array_diff($required, $data_keys) == array_diff($data_keys, $required);
+    }
+}
+
+if (!function_exists("api_logger")) {
+    function api_logger($app, string $methods, array $params){ //, $func){
+        /**
+         * Display info / warning / error front api request
+         *
+         * @param Object $app Slim app instance
+         * @param string $methods GET / POSt / PUT / DELETE
+         * @param array $params $func parameters
+         * @param function $func to execute
+         *
+         * @return array $res result of $func
+         */
+        $app->logger->info($methods." ".json_encode($params));
+        include "../src/resources/File.php";
+        $file = new File();
+        $res = call_user_func_array(array($file, strtolower($methods)), $params);
+        // $res = $func($params);
+        if ($res[1] % 500 < 100)
+            $app->logger->error("[".$res[1]."] ".$res[0]['message']);
+        else if ($res[1] % 400 < 100)
+            $app->logger->warning("[".$res[1]."] ".$res[0]['message']);
+        else
+            $app->logger->info("[".$res[1]."] ".(array_key_exists('message', $res) ? $res[0]['message'] : ""));
+
+        return $res;
+    }
 }
